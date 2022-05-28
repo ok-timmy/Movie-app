575098663c0f9e05ac91881636ef72495253fc52
Timmy <oktimmy45@gmail.com>
2022-05-28 13:55:04 +0100

    Almost Done

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,4 +1,4 @@
-import firebase, {app} from "../../src/config/firebase.config";
+import firebase, { app, db } from "../../src/config/firebase.config";
 import {
   getAuth,
   signInWithEmailAndPassword,
@@ -6,80 +6,82 @@ import {
   signInWithPopup,
   createUserWithEmailAndPassword,
 } from "firebase/auth";
-import { doc, setDoc } from "firebase/firestore"; 
+import { collection, doc, setDoc } from "firebase/firestore";
+import { GetUserData } from "./fetchUser";
 
+const adduser = async (n, e, img) => {
+  await setDoc(doc(collection, "users", e), {
+    name: n,
+    email: e,
+    profilePicture: img || "",
+    favouritesMovies: [],
+  });
+};
 
-
-
-const adduser = async(n, e, img) => {
-    await setDoc(doc("users", "users", e), {
-      name: n,
-      email: e,
-      profilePicture: img || "",
-      favouritesMovies: [],
+export function SignInWithEmail(_email, _password, router) {
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, _email, _password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      GetUserData(user.email);
+      login(user, user.providerId )
+      router.fallback();
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
     });
-  }
+}
 
+export function SignUpWithEmail(_name, _email, _password, router) {
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, _email, _password)
+    .then((userCredential) => {
+      // Signed in
+      // const user = userCredential.user;
+      console.log(userCredential);
+      adduser(_name, _email); // Create a new Document for the new user to the database
+      router.push("/account/sign-in");
+    })
+    .catch((error) => {
+      console.log(error.code);
+      console.log(error.message);
+      // const errorCode = error.code;
+      // const errorMessage = error.message;
+    });
+}
 
-export function SignInWithEmail (_email, _password, router) {
-    const auth = getAuth();
-    signInWithEmailAndPassword(auth, _email, _password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        router.back();
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage)
-      });
-  };
+export function SignedUpWithGoogle(router, login) {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
 
-  export function SignUpWithEmail (_name, _email, _password, router) {  
-    const auth = getAuth(); 
-    createUserWithEmailAndPassword(auth, _email, _password)
-      .then((userCredential) => {
-        // Signed in
-        // const user = userCredential.user;
-        console.log(userCredential);
-         adduser(_name, _email);
-        router.push("/account/sign-in");
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
-  };
-
-  export function SignedUpWithGoogle(router, login) {
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth();
-    
-    signInWithPopup(auth, provider)
+  signInWithPopup(auth, provider)
     .then((result) => {
-        console.log(auth);
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        console.log(token);
-        console.log(user);
-       login(user, token);
-        router.back();
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        console.log(errorMessage);
-        // ...
-      });
-  };
+      // console.log(auth);
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // console.log(token);
+      console.log(user);
+      GetUserData(user.email, adduser(user.displayName, user.email, user.photoURL));
+      login(user, token);
+
+      router.fallback();
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      console.log(errorMessage);
+      // ...
+    });
+}
